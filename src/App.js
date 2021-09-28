deb628cb53d7ace81fd1f1effd8fffef3aee6f8e
clec <clecobby@gmail.com>
2021-09-28 22:56:32 +0000

    construction page done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
+import React from "react";
+import ReactDOM from "react-dom";
+import Preloader from "./components/Preloader/Preloader";
+import Timer from "./components/Countdown/Timer";
+import Optin from "./components/Optin/Optin";
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and savereee to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>
+         Bacchus Tech Website
+          <br />
+          Coming Soon
+        </h1>
+        <Timer />
+        <Optin />
+        <Preloader />
+      </div>
     </div>
   );
 }
