a92ebe62b49cba5f4e6b0070c0457dc9fc5f87f8
Andy-COD <aahwireng10@gmial.com>
2023-03-11 09:03:51 +0000

    Added content to about page

---
+++ src/components/About Page/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <Section>
+        <div>
+            <img src="../../../public/assets/images/logo3.png" alt="Logo" />
+        </div>
+    </Section>
+  )
+}
+
+export default About
