1f6132498384282af71b88ea3cdceb90ff2ab836
Andy-COD <aahwireng10@gmial.com>
2023-03-11 11:27:33 +0000

    none

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,9 +6,9 @@ function NavBar() {
       <h3>Logo</h3>
       <nav>
         <a href='./Home'>Home</a>
-        <a href='./About'>About</a>
+        <a href='./About'>About Us</a>
         <a href='./Services'>Services</a>
-        <a href='./Contact'>Contact Us</a>
+        <a href='./Contact'>Contact</a>
       </nav>
     </header>
   )
