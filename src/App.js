05d6063d89f31fdd802041c8b91c2b5245cf98ae
Andy-COD <aahwireng10@gmial.com>
2023-03-11 11:25:57 +0000

    Replaced project src folder

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from "react";
-
 import './App.css';
-// import NavBar from './components/NavBar';
+import React from 'react';
+import NavBar from './components/NavBar';
 
 function App() {
+  
   return (
-    <main>
-      <p>Hello</p>
-    </main>
+    <React.Fragment>
+      <NavBar />
+    </React.Fragment>
   );
 }
 
---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <header>
+      <h3>Logo</h3>
+      <nav>
+        <a href='./Home'>Home</a>
+        <a href='./About'>About</a>
+        <a href='./Services'>Services</a>
+        <a href='./Contact'>Contact Us</a>
+      </nav>
+    </header>
+  )
+}
+
+export default NavBar
