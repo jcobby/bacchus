699e21d12e249e80d33e3c6bd977291ac5898367
Andy-COD <aahwireng10@gmial.com>
2023-03-11 11:22:28 +0000

    Deleted old modules

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
-import logo from './logo.svg';
 import React from "react";
-import ReactDOM from "react-dom";
-import Preloader from "./components/Preloader/Preloader";
-import Timer from "./components/Countdown/Timer";
-import Optin from "./components/Optin/Optin";
 
 import './App.css';
 import NavBar from './components/NavBar';
